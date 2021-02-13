b2046d3fe74fe14345c1abebba1d7e4962a98f8e
Admin <raghav404219@gmail.com>
2021-02-13 21:08:57 +0530

    Updated sql queries

---
+++ api/management/commands/sql/create_and_populate_tmp_password.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE IF NOT EXISTS tmp_api_password (
+    id SERIAL,
+    hash character varying(100) UNIQUE NOT NULL,
+    count integer NOT NULL
+);
+TRUNCATE TABLE tmp_api_password;
+
+BEGIN;
+    INSERT INTO tmp_api_password(hash,count)
+    SELECT hash,count(hash)
+    FROM tmp group by(hash);
+commit;
+
--- api/management/commands/sql/populate_password_table.sql
+++ api/management/commands/sql/populate_password_table.sql
@@ -1,7 +1,21 @@
 
+--BEGIN;
+--    INSERT INTO api_password(hash,count)
+--    SELECT hash,count(hash)
+--    FROM tmp group by(hash)
+--    ON CONFLICT (hash) DO UPDATE SET count=(Select count from api_password where hash=excluded.hash)+excluded.count;
+--commit;
+--BEGIN;
+--    INSERT INTO api_password(hash,count)
+--    SELECT tmp_api_password.hash,tmp_api_password.count
+--    FROM tmp_api_password,api_password
+--    WHERE tmp_api_password.hash <> api_password.hash;
+--COMMIT;
+
+--Inserting records from tmp_api_password which doesn't exist in api_password
 BEGIN;
     INSERT INTO api_password(hash,count)
-    SELECT hash,count(hash)
-    FROM tmp group by(hash)
-    ON CONFLICT (hash) DO UPDATE SET count=(Select count from api_password where hash=excluded.hash)+excluded.count;
-commit;
+    SELECT tmp_api_password.hash,tmp_api_password.count
+    FROM tmp_api_password LEFT JOIN api_password ON tmp_api_password.hash=api_password.hash
+    WHERE api_password.hash IS NULL;
+COMMIT;
